2df0a54d8132933efd4fcd773a4cbb3fb2e39748
Chawki <chawkibensalem5@gmail.com>
2023-04-09 14:52:29 +0100

    get_page function added is_form function added requests and lxml are added to requirements.txt

--- main.py
+++ main.py
@@ -3,8 +3,35 @@
 import typer
 import whois
 import nmap3
+import requests
+from lxml import html
 
 app = typer.Typer()
+
+def get_page(url: str, proxy: str=None):
+    """Perform a GET request and return a response object."""
+    proxies = None
+    if proxy:
+        proxies = {"http": f"http://{proxy}"}
+    response = requests.get(url, proxies = proxies)
+    return response
+
+@app.command()
+def is_form(url: str, proxy: str=None):
+    """Find a form in a page, and print form details."""
+    response = get_page(url,proxy)
+    tree = html.fromstring(response.content)
+    for form in tree.xpath("//form"):
+        print(f"Found a {form.method} form for {form.action}")
+        for field in form.fields:
+            print(f"Contains input field {field}")
+#            choice = input("Would you like to extract data from the form ? (Y / N)")
+#            while choice not in ['y','y','N','n']:
+#                print("invalid choice")
+#                choice = input("Would you like to extract data from the form ? (Y / N)")
+#                if choice in ['Y','y']:
+
+
 
 
 @app.command()
@@ -12,6 +39,7 @@ def domain_lookup(name: str):
     """Print the domain resgistrant's name and orgranization"""
     results = whois.whois(name)
     print(f"{name} is registered by {results.name} - {results.org}")
+
 
 @app.command()
 def port_scan(target: str, top: int=10):
